bff22fd71b0581748c7f3bc9ec15e2ac7dbbe173
jgirard <girard.julien@gmail.com>
2020-08-14 14:15:07 +0200

    Init project with Spring Initializr

---
+++ src/main/java/org/transactions/transactionssyncprocess/TransactionsSyncProcessApplication.java
@@ -0,0 +1,13 @@
+package org.transactions.transactionssyncprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsSyncProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsSyncProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/transactions/transactionssyncprocess/TransactionsSyncProcessApplicationTests.java
@@ -0,0 +1,15 @@
+package org.transactions.transactionssyncprocess;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class TransactionsSyncProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
